f3084bc0ef25672c71bce922bdf9c779c9dc04ba
WataruSuzuki <wataru0406@gmail.com>
2016-11-30 10:40:14 +0900

    Initial Commit

---
+++ RevocationAlamofire/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RevocationAlamofire
+//
+//  Created by WataruSuzuki on 2016/11/30.
+//  Copyright © 2016年 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
